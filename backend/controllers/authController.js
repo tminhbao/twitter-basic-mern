3913a896a358a54d78429ed6a0927c2c7c3d1920
tminhbao <tminhbao2001@gmail.com>
2023-02-28 19:13:54 +0700

    create auth route, auth controller

---
+++ backend/controllers/authController.js
@@ -0,0 +1,7 @@
+exports.register = (req, res, next) => {
+  res.json("User is registered successfully");
+};
+
+exports.login = (req, res, next) => {
+  res.json("User is logged in successfully");
+};
