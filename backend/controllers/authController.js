2956efcc851e92dacfb158a76cba1f137caa693e
tminhbao <tminhbao2001@gmail.com>
2023-02-28 23:20:33 +0700

    hash password and create token key

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,18 +1,48 @@
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res, next) => {
   try {
     // req.body = email, name, password
-    const user = await User.create(req.body);
-    res.status(200).json({
-      status: "success",
-      data: { user },
+    bcrypt.hash(req.body.password, 10, async (error, hashPassword) => {
+      if (error) return next(error);
+      else {
+        const user = await User.create({
+          email: req.body.email,
+          name: req.body.name,
+          password: hashPassword,
+        });
+        const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+        res.status(200).json({
+          status: "success",
+          data: { token, user },
+        });
+      }
     });
   } catch (error) {
+    console.log(error);
     res.json(error);
   }
 };
 
-exports.login = (req, res, next) => {
-  res.json("User is logged in successfully");
+exports.login = async (req, res, next) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      // Error:  Email is not rìght
+    }
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      const token = jwt.sign({ userId: user._id }, process.env.SECRET_KEY);
+      res.status(200).json({
+        status: "success",
+        data: {
+          token,
+          user,
+        },
+      });
+    } else {
+      // Error: Password is not correct
+    }
+  } catch (error) {}
 };
--- backend/models/User.js
+++ backend/models/User.js
@@ -19,23 +19,22 @@ const userSchema = new mongoose.Schema(
       type: String,
       trim: true,
       require: [true, "Password must be required"],
-      minlength: [6, "Password must be at least 6 characters"],
     },
   },
   { timestamps: true }
 );
 
-userSchema.pre("save", (next) => {
-  let user = this;
-  bcrypt.hash(user.password, 10, (error, hash) => {
-    if (error) {
-      return next(error);
-    } else {
-      user.password = hash;
-      next();
-    }
-  });
-});
+// userSchema.pre("save", (next) => {
+//   let user = this;
+//   bcrypt.hash(user.password, 10, (error, hash) => {
+//     if (error) {
+//       return next(error);
+//     } else {
+//       user.password = hash;
+//       next();
+//     }
+//   });
+// });
 
 const User = mongoose.model("User", userSchema);
 
